747aed9ccfcbd3e8780a8ac43027b437977507c4
kouzze01 <38171916+kouzze01@users.noreply.github.com>
2018-04-08 15:27:17 +0700

    update include

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 
-include ('line-bot-api/php/line-bot.php');
+include ('line-bot-api-master/php/line-bot.php');
 
 $channelSecret = '9fcdaf0e6d3368f22872ed09751c1f99';
 $access_token  = 'LGHmeErIoWEVY15w4l5sHPIKhEDSlf2Q8HQ77pBMEKwu9l5DQ4A0ZTepyDD4DnGR6JuIqU6tVtNhBmHL+oF+OVj9HCPXZz0doP0cxmY9LpdslVwyzEIerZWbNt7gYkVmjBYgB7F0fYxufc5Ux0VNgAdB04t89/1O/w1cDnyilFU=';
