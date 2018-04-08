20b87da4a2400886009dcf72f3d53db6bb9edf13
kouzze01 <38171916+kouzze01@users.noreply.github.com>
2018-04-08 15:25:38 +0700

    update Token & Chanel Secret

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 
 include ('line-bot-api/php/line-bot.php');
 
-$channelSecret = '0072137cb0a8054eab09dc0247413515';
+$channelSecret = '9fcdaf0e6d3368f22872ed09751c1f99';
 $access_token  = 'LGHmeErIoWEVY15w4l5sHPIKhEDSlf2Q8HQ77pBMEKwu9l5DQ4A0ZTepyDD4DnGR6JuIqU6tVtNhBmHL+oF+OVj9HCPXZz0doP0cxmY9LpdslVwyzEIerZWbNt7gYkVmjBYgB7F0fYxufc5Ux0VNgAdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
